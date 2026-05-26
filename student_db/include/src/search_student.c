3a81bbcff365a24931fb57437c4d322c7e3ff7e9
HARIPRASATH R <hariraja1812@gmail.com>
2026-05-26 09:35:44 +0530

    Add files via upload

--- student_db/include/src/search_student.c
+++ student_db/include/src/search_student.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include "../include/student.h"
 
+void search_student() {
+
+    FILE *fp = fopen("students.txt", "r");
+
+    int id;
+    Student s;
+    int found = 0;
+
+    printf("Enter ID to search: ");
+    scanf("%d", &id);
+
+    while(fscanf(fp, "%d %s", &s.id, s.name) != EOF) {
+
+        if(s.id == id) {
+            printf("Found: %d %s\n", s.id, s.name);
+            found = 1;
+        }
+    }
+
+    if(!found)
+        printf("Student not found\n");
+
+    fclose(fp);
+}
