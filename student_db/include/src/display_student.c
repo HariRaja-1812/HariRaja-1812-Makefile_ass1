53719f75c47f21930e188f5e22d86514f0c728c8
HARIPRASATH R <hariraja1812@gmail.com>
2026-05-26 09:36:44 +0530

    Add files via upload

--- student_db/include/src/display_student.c
+++ student_db/include/src/display_student.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include "../include/student.h"
 
+void display_students() {
+
+    FILE *fp = fopen("students.txt", "r");
+
+    Student s;
+
+    while(fscanf(fp, "%d %s", &s.id, s.name) != EOF) {
+        printf("%d %s\n", s.id, s.name);
+    }
+
+    fclose(fp);
+}
