5e797b14b09de0dfd7099afef7c1d2b2fcb1faa0
HARIPRASATH R <hariraja1812@gmail.com>
2026-05-26 09:29:34 +0530

    Add files via upload

--- student_db/include/student.h
+++ student_db/include/student.h
@@ -1,1 +1,13 @@
+#ifndef STUDENT_H
+#define STUDENT_H
 
+typedef struct {
+    int id;
+    char name[50];
+} Student;
+
+void add_student();
+void search_student();
+void display_students();
+
+#endif
