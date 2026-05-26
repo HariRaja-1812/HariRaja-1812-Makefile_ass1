7fc83d3dde2b00c28cd31626d2de972784d48eb9
HARIPRASATH R <hariraja1812@gmail.com>
2026-05-26 09:34:16 +0530

    Add files via upload

--- student_db/include/src/add_student.c
+++ student_db/include/src/add_student.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include "../include/student.h"
 
+void add_student() {
+    FILE *fp = fopen("students.txt", "a");
+
+    Student s;
+
+    printf("Enter ID: ");
+    scanf("%d", &s.id);
+
+    printf("Enter Name: ");
+    scanf("%s", s.name);
+
+    fprintf(fp, "%d %s\n", s.id, s.name);
+
+    fclose(fp);
+
+    printf("Student Added Successfully\n");
+}
